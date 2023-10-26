4ceb79f92e2a13e46e2a912d80a0eaaf778c2b1c
larcondo <larcondo90@gmail.com>
2023-10-26 18:58:22 -0300

    exercise 9.18

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,39 @@ import { DiaryEntry } from './types';
 import { getAllDiaries, createNew } from './services/diaryService';
 import Diary from './components/Diary';
 import NewDiaryForm from './components/NewDiaryForm';
+import Notification from './components/Notification';
+import axios from 'axios';
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   useEffect(() => {
     getAllDiaries().then(data => setDiaries(data));
   }, []);
 
-  const createDiary = (newEntry: DiaryEntry) => {
-    createNew(newEntry).then(data => setDiaries(diaries.concat(data)));
+  const createDiary = async (newEntry: DiaryEntry) => {
+    // createNew(newEntry).then(data => setDiaries(diaries.concat(data)));
+    createNew(newEntry)
+      .then(data => setDiaries(diaries.concat(data)))
+      .catch( err => {
+        if (axios.isAxiosError(err)) {
+          // console.log(err.response?.data);
+          setErrorMsg(err.response?.data);
+          setTimeout(() => setErrorMsg(null), 2000);
+        } else {
+          console.error(err);
+          setErrorMsg('Unexpected error. Try later');
+          setTimeout(() => setErrorMsg(null), 2000);
+        }
+      });
   };
 
   return (
     <>
       <h1>Ilari's flight diaries</h1>
       <h2>Add new entry</h2>
+      <Notification message={errorMsg} />
 
       <NewDiaryForm newDiaryFn={createDiary} />
 
---
+++ src/components/Notification.tsx
@@ -0,0 +1,11 @@
+const Notification = ({ message }: { message: string | null }) => {
+  if (!message) return null;
+  
+  return(
+    <div>
+      <p style={{ color: 'red' }}>{ message }</p>
+    </div>
+  );
+};
+
+export default Notification;
